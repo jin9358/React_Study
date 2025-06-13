7a988d09822fb19fa0aee0a64a81ec40b41b8df9
jin9358 <leejh9358@naver.com>
2025-06-13 15:37:16 +0900

    feat: 20250613 route sutdy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import Router1 from './RouterStudy/Router1/Router1'
 import Effect1 from './study/components/Effect/Effcet1/Effect1'
 import Effect2 from './study/components/Effect/Effect2/Effect2'
 import Emotion from './study/components/Emotion/Emotion'
@@ -14,10 +16,13 @@ import InputState2 from './study/components/State/InputState2/InputState2'
 import InputState3 from './study/components/State/InputState3/InputState3'
 import InputState4 from './study/components/State/InputState4/InputState4'
 import Index from './TodoList/pages/Index'
+import Router2 from './RouterStudy/Router2/Router2'
+import Router3 from './RouterStudy/Router3/Router3'
+import Router4 from './RouterStudy/Router4/Router4'
 
 function App() {
 
-  return <div>
+  return <BrowserRouter>
     {/* <HelloReact/> */}
     {/* <HelloJsx /> */}
     {/* <HelloProps/> */}
@@ -26,14 +31,18 @@ function App() {
     {/* <InputState1 /> */}
     {/* <InputState2 /> */}
     {/* <InputState3/> */}
-    {/* <InputState4/> */}
+    {/* <InputState4/> */}  
     {/* <DomRef/> */}
     {/* <Effect1/> */}
     {/* <Effect2/> */}
     {/* <Emotion/> */}
     {/* <Emotion2/> */}
-    <Index />
-  </div>
+    {/* <Index /> */}
+    {/* <Router1 /> */}
+    {/* <Router2 /> */}
+    <Router3 />
+    {/* <Router4/> */}
+  </BrowserRouter>
 }
 
 export default App
